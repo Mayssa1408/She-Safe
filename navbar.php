d042413ebd58106f509f60373937f9222653de4a
Mayssa <58940@etu.he2b.be>
2024-12-02 15:54:35 +0100

    Ajoutes le repository

---
+++ navbar.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container-fluid">
+    <!-- Logo -->
+    <a class="navbar-brand fs-4" href="#"><h1>She Safe</h1></a>
+    <!-- Toggle Button -->
+    <button
+      class="navbar-toggler btn-white shadow-none border-0"
+      type="button"
+      data-bs-toggle="offcanvas"
+      data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Sidebar -->
+    <div
+      class="sidebar offcanvas offcanvas-start"
+      tabindex="-1"
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+    >
+      <div class="offcanvas-header text-white border-bottom">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+        <button
+          type="button"
+          class="btn-close btn-close-white shadow-none"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
+        <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#safePlace">Safe Place</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#forum">Forum</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#supportAide">Support et Aide</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#apropos">À propos</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="#moncompte">Mon compte</a>
+          </li>
+        </ul>
+        <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
+          <a href="#monCompte" class="text-white">Se connecter</a>
+          <a href="#monCompte" class="text-white text-decoration-none px-3 py-1 rounded-4" style="background-color: #b7536c">
+            S'inscrire
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
