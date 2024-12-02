decb5f0b3e98bd23a56b071e687e5d0a1980b557
Mayssa <58940@etu.he2b.be>
2024-12-02 16:39:18 +0100

    init wordpress

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>She Safe</title>
-  </head>
-  <body class="d-flex flex-column vh-100">
-    <?php include 'includes/navbar.php'; ?>
-    <main class="flex-grow-1">
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
